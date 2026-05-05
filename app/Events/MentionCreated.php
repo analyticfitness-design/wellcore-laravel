36ad5867f50dcfb7dc6beeda8ae183d73653de3a
WellCore Fitness <info@wellcorefitness.com>
2026-05-05 10:03:58 -0500

    feat(community): MentionCreated broadcast event (replace stub)

--- app/Events/MentionCreated.php
+++ app/Events/MentionCreated.php
@@ -2,8 +2,16 @@
 
 namespace App\Events;
 
-class MentionCreated
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MentionCreated implements ShouldBroadcast
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public function __construct(
         public ?int $postId,
         public ?int $commentId,
@@ -12,4 +20,25 @@ class MentionCreated
         public string $mentionedType,
         public int $mentionedId,
     ) {}
+
+    public function broadcastOn(): PrivateChannel
+    {
+        return new PrivateChannel("user.{$this->mentionedType}.{$this->mentionedId}");
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'mention-created';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'post_id'        => $this->postId,
+            'comment_id'     => $this->commentId,
+            'mentioner_type' => $this->mentionerType,
+            'mentioner_id'   => $this->mentionerId,
+            'at'             => now()->toIso8601String(),
+        ];
+    }
 }
