e643a5eb880121c7e366f83242bfcb47cce0a844
WellCore Fitness <info@wellcorefitness.com>
2026-05-05 09:30:43 -0500

    feat(community): factories for PinnedPost, PostReport, BroadcastMessage

--- app/Models/BroadcastMessage.php
+++ app/Models/BroadcastMessage.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BroadcastMessage extends Model
 {
+    use HasFactory;
+
     protected $table = 'broadcast_messages';
 
     public $timestamps = false;
--- app/Models/PostReport.php
+++ app/Models/PostReport.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PostReport extends Model
 {
+    use HasFactory;
+
     protected $table = 'post_reports';
 
     public $timestamps = false;
