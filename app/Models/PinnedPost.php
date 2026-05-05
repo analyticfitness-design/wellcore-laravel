7ad8181bc4e3722a3b6295d883a4f36dd50cf9c2
WellCore Fitness <info@wellcorefitness.com>
2026-05-05 09:26:59 -0500

    feat(community): PinnedPost model

---
+++ app/Models/PinnedPost.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PinnedPost extends Model
+{
+    protected $table = 'pinned_posts';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'post_id',
+        'pinned_by_type',
+        'pinned_by_id',
+        'pinned_at',
+        'pinned_until',
+        'note',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'pinned_at' => 'datetime',
+            'pinned_until' => 'datetime',
+        ];
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(CommunityPost::class, 'post_id');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where(function ($q) {
+            $q->whereNull('pinned_until')->orWhere('pinned_until', '>', now());
+        });
+    }
+}
