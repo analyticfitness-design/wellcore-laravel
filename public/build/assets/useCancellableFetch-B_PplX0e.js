fcef0bc42ab5d76306b639292700d039db38cf15
WellCore Fitness <info@wellcorefitness.com>
2026-05-05 01:02:04 -0500

    build(food-photos): rebuild assets post-merge main

---
+++ public/build/assets/useCancellableFetch-B_PplX0e.js
@@ -0,0 +1,1 @@
+import{U as e}from"./vue-core-CRfPG3JC.js";function t(){let t=null;function n(){t?.abort(),t=null}function r(){return n(),t=new AbortController,t.signal}return e(n),{cancel:n,getSignal:r}}export{t};
